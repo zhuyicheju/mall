c6a10b63af5a29fb094a2f356f579df8a3d46b40
zhuyicheju <@>
2025-11-04 11:13:44 +0800

    init

---
+++ src/main/java/com/example/myself_mall/MyselfMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.myself_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyselfMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyselfMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myself_mall/MyselfMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myself_mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyselfMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
